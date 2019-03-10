6eaf622ebfd64bdf552398185f4ed961f136da36
Daniil Eliseev <daniileliseev45@gmail.com>
2019-03-10 16:51:00 +0000

    Tweaks

--- etc/gunicornask.py
+++ etc/gunicornask.py
@@ -3,9 +3,9 @@ from math import ceil
 # Socket, Threads number, Python Path
 bind = "0.0.0.0:8000"
 workers = ceil((cpu_count() * 2 + 1)/2)
-pythonpath = "/vagrant/web2/ask"
+pythonpath = "/vagrant/web/ask"
 # Logs
 loglevel = 'debug'
-accesslog = '/vagrant/web2/etc/logs/ask_access.log'
+accesslog = '/vagrant/web/etc/logs/ask_access.log'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog = '/vagrant/web2/etc/logs/ask_error.log'
+errorlog = '/vagrant/web/etc/logs/ask_error.log'
